8b9da8c20e98ddaf341afda01dc9eb9797df172b
Sankar <sankar@learnsomuch.com>
2016-01-21 18:46:01 +0100

    Initialise variables for uppercase, lowercase, numbers, symbols and passing to function as argument

--- all.js
+++ all.js
@@ -3,39 +3,59 @@
 // Docs & Web: https://learnsomuch.github.io/all.js
 
 ;(function() {
-  // Initializing the main object as a window in the browser
+  	// Initializing the main object as a window in the browser
 	var main = this;
 	//	defining an object
 	var all = {};
-  //	Print version of this all.js
-  all.version = '0.0.1';
+  	//	Print version of this all.js
+  	all.version = '0.0.1';
 	//	all object set global to the main object as a window
-  main.all = all;
+ 	main.all = all;
 
-  // Generate random string with the length passed as an arugement to function
-	all.gstring = function gstring(length) {
-    var output = "";
-    var available = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+ 	// API for Generate
 
-    for (var i=0; i<length; i++) {
-      output = output + available.charAt(Math.floor(Math.random() * available.length));
-    }
-    return output;
-  }
+ 	// Initialising uppercase, lowercase chars, numbers, symbols and combination of others.
+ 	all.u = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+ 	all.l = 'abcdefghijklmnopqrstuvwxyz';
+ 	all.n = '0123456789';
+ 	all.s = '~`!@#$%^&*()_+-={}[]:";\'<>?,./|\\';
+ 	all.ul = u + l;
+ 	all.un = u + n;
+ 	all.us = u + s;
+ 	all.ln = l + n;
+ 	all.ls = l + s;
+ 	all.ns = n + s;
+ 	all.uln = u + l + n;
+ 	all.ulns = u + l + n + s;
 
-  // Generate random integer with the length passed as an argument to function (max. 21)
-  all.gnum = function gnum(length) {
-    return Math.floor(Math.pow(10, length-1) + Math.random() * (Math.pow(10, length) - Math.pow(10, length-1) - 1));
-  }
+  	// Generate random string with the length and input passed as an arugement to function
+  	// For eg. all.gstring(21, all.ulns); // output will be "D*Iz?'LfLM"fW\d%`iSE"" input can be single or combination of others
+	all.gstring = function gstring(length, input) {
+    	var output = "";
+    	var available = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  // Generate random integer within a range or below a certain numner. This can be passed as an argument
-  all.gint = function gint(a, b) {
-  if (arguments.length === 1) {
-    b = a;
-    a = 0;
-  }
-  return Math.floor(Math.random() * (b - a + 1) + a);
-}
+	    for (var i=0; i<length; i++) {
+	      output = output + input.charAt(Math.floor(Math.random() * input.length));
+	    }
+	    return output;
+  	}
+
+  	// Generate random integer with the length passed as an argument to function (max. 21)
+  	all.gnum = function gnum(length) {
+    	return Math.floor(Math.pow(10, length-1) + Math.random() * (Math.pow(10, length) - Math.pow(10, length-1) - 1));
+  	}
+
+  	// Generate random integer within a range or below a certain numner. This can be passed as an argument
+  	all.gint = function gint(a, b) {
+  		if (arguments.length === 1) {
+    		b = a;
+    		a = 0;
+  		}
+  		return Math.floor(Math.random() * (b - a + 1) + a);
+  	}
+
+  	// Generate random alpha-numberic string with the length passed as an argument.
+  	// Considering strings with uppercase lowe
 
 }.call(this));
 // End of all.js
